06da42cc2e231acb96a4b80bd23521e42fa5e63b
ouazzaniredouane <r.ouazzani.chahdi1986@gmail.com>
2025-05-13 17:47:40 +0100

    Initial commit - Nextjs-dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lora, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lora = Lora({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: '400'});
